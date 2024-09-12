dbfc3216833058fe75399083f9a1450ae10ac701
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2024-09-11 23:25:28 -0300

    feat: cors

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -8,8 +8,13 @@ import { createGoalRoute } from './routes/create-goal'
 import { createGoalCompletionRoute } from './routes/create-goal-completion'
 import { getWeekGoalsRoute } from './routes/get-week-goals'
 import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
